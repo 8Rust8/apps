cf20ecceea5a774ba1efd5d1040e5d360a1e0821
5learn5 <try.vsb@gmail.com>
2023-04-26 19:58:39 +0530

    Combining structs to handel multiple states

--- yew/todo/src/components/molecules/custom_form.rs
+++ yew/todo/src/components/molecules/custom_form.rs
@@ -1,3 +1,6 @@
+use std::clone;
+use std::ops::Deref;
+
 use crate::components::atoms::custom_button::CustomButton;
 use crate::components::atoms::main_title::{Color, MainTitle};
 use crate::components::atoms::text_input::TextInput;
@@ -6,28 +9,54 @@ use wasm_bindgen::JsCast;
 use web_sys::{Event, HtmlInputElement};
 use yew::prelude::*;
 
+#[derive(Default, Clone)]
+struct State {
+    username: String,
+    language: String,
+    count: u32,
+}
+
 #[function_component(CustomForm)]
 pub fn custom_form() -> Html {
-    let user_name_state = use_state(|| "No name set".to_string());
-    let click_count_state = use_state(|| 0_u32);
-    
-    let cloned_user_name_state = user_name_state.clone();
+    let state = use_state(|| State::default());
+
+    let cloned_state = state.clone();
     let username_changed = Callback::from(move |username: String| {
-        log!("Username changed in custom form" , &username);
-        cloned_user_name_state.set(username.to_string());
+        cloned_state.set(State {
+            username,
+            ..cloned_state.deref().clone()
+        });
     });
 
-    let colned_click_count_state = click_count_state.clone();
+    let cloned_state = state.clone();
+    let language_changed = Callback::from(move |language: String| {
+        cloned_state.set(State {
+            language,
+            ..cloned_state.deref().clone()
+        });
+    });
+
+    let cloned_state = state.clone();
     let handel_button_click = Callback::from(move |_| {
-        colned_click_count_state.set(*colned_click_count_state + 1);
+        let count = cloned_state.count + 1;
+        cloned_state.set(State {
+            count,
+            ..cloned_state.deref().clone()
+        })
     });
 
     html! {
         <div>
-            <TextInput name={"Username"} handel_onchange={username_changed} />
+            <div style="display: flex">
+                <p>{"Username "} <TextInput name={"Username"} handel_onchange={username_changed} /></p>
+                <p>{"Language "} <TextInput name={"Language"} handel_onchange={language_changed} /></p>
+            </div>
+            <div>
+                <p>{"Username : "}{&state.username}</p>
+                <p>{"Language : "}{&state.language}</p>
+                <p>{"Button clicked for "}{&state.count}{ " of times"}</p>
+            </div>
             <CustomButton label="Click Here!!" onclick = {handel_button_click}/>
-            <p>{"User Name : "}{&*user_name_state}</p>
-            <p>{"Button clicked for "}{&*click_count_state}{ " of times"}</p>
         </div>
     }
 }
