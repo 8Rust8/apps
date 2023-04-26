7ef8ab4eb60dbdc258cd776f1de5c01bd0256e4a
5learn5 <try.vsb@gmail.com>
2023-04-26 17:10:12 +0530

    states using individual use_states

--- yew/todo/src/components/atoms/custom_button.rs
+++ yew/todo/src/components/atoms/custom_button.rs
@@ -4,13 +4,18 @@ use yew::prelude::*;
 #[derive(Properties, PartialEq)]
 pub struct Props {
     pub label: String,
+    pub onclick: Callback<()>
 }
 
 #[function_component(CustomButton)]
 pub fn custom_button(props: &Props) -> Html {
+    let cloned_click = props.onclick.clone();
+    let onclick = Callback::from(move |_| {
+        cloned_click.emit(());
+    });
     html! {
         <>
-            <button name="custombutton">{&props.label.clone()}</button>
+            <button name="custombutton" onclick={onclick}>{&props.label.clone()}</button>
         </>
     }
 }
--- yew/todo/src/components/molecules/custom_form.rs
+++ yew/todo/src/components/molecules/custom_form.rs
@@ -1,7 +1,6 @@
 use crate::components::atoms::custom_button::CustomButton;
 use crate::components::atoms::main_title::{Color, MainTitle};
 use crate::components::atoms::text_input::TextInput;
-use gloo::console::externs::log;
 use gloo::console::log;
 use wasm_bindgen::JsCast;
 use web_sys::{Event, HtmlInputElement};
@@ -10,7 +9,7 @@ use yew::prelude::*;
 #[function_component(CustomForm)]
 pub fn custom_form() -> Html {
     let user_name_state = use_state(|| "No name set".to_string());
-    let custom_form_main_title_cb = Callback::from(|message| log!(message, "for custom form"));
+    let click_count_state = use_state(|| 0_u32);
     
     let cloned_user_name_state = user_name_state.clone();
     let username_changed = Callback::from(move |username: String| {
@@ -18,12 +17,17 @@ pub fn custom_form() -> Html {
         cloned_user_name_state.set(username.to_string());
     });
 
+    let colned_click_count_state = click_count_state.clone();
+    let handel_button_click = Callback::from(move |_| {
+        colned_click_count_state.set(*colned_click_count_state + 1);
+    });
+
     html! {
-        <form>
-            <MainTitle title={"Yewwww"} color={Color::Normal} on_load={custom_form_main_title_cb} />
-            <TextInput name={"Another elephant"} handel_onchange={username_changed} />
-            <CustomButton label="Click Here!!"/>
+        <div>
+            <TextInput name={"Username"} handel_onchange={username_changed} />
+            <CustomButton label="Click Here!!" onclick = {handel_button_click}/>
             <p>{"User Name : "}{&*user_name_state}</p>
-        </form>
+            <p>{"Button clicked for "}{&*click_count_state}{ " of times"}</p>
+        </div>
     }
 }
